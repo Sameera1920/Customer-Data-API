36151c679a9bd1dc02816adc2d9e29fc2041866e
Sameera1920 <sameera1920@gmail.com>
2023-06-21 12:11:16 +0530

    Take Address class in to a separate file

--- TestApp1/Models/Address.cs
+++ TestApp1/Models/Address.cs
@@ -7,21 +7,16 @@ using System.Threading.Tasks;
 
 namespace TestApp1.Models
 {
-    public class User
+
+    public class Address
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int Index { get; set; }
-        public int Age { get; set; }
-        public string? EyeColor { get; set; }
-        public string? Name { get; set; }
-        public string? Gender { get; set; }
-        public string? Company { get; set; }
-        public string? Email { get; set; }
+        public int Number { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
         public string? Phone { get; set; }
-        public Address? Address { get; set; }
-
+        public int Zipcode { get; set; }
     }
-
 }

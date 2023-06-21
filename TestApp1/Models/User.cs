503e5d80d5b7ed60886c4348dd51233909516420
Sameera1920 <sameera1920@gmail.com>
2023-06-21 12:07:43 +0530

    migration files make tracked by git

--- TestApp1/Models/User.cs
+++ TestApp1/Models/User.cs
@@ -27,10 +27,12 @@ namespace TestApp1.Models
     public class Address
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public int Number { get; set; }
-        public string? Company { get; set; }
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
         public int Zipcode { get; set; }
     }
 }
